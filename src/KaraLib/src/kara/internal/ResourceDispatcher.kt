2d041f136863d90e6047a7d5b7eb8da5a7abe851
denis.linkov <Denis.Linkov@jetbrains.com>
2018-02-15 12:52:56 +0300

    Fail on init if duplicated routes are found

--- src/KaraLib/src/kara/internal/ResourceDispatcher.kt
+++ src/KaraLib/src/kara/internal/ResourceDispatcher.kt
@@ -1,10 +1,8 @@
 package kara.internal
 
 import kara.ApplicationContext
-import kara.HttpMethod
 import kara.InvalidRouteException
 import kara.asHttpMethod
-import java.util.*
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 import kotlin.reflect.KAnnotatedElement
@@ -12,15 +10,12 @@ import kotlin.reflect.KAnnotatedElement
 /** Used by the server to dispatch requests to their appropriate actions.
  */
 class ResourceDispatcher(val context: ApplicationContext, resourceTypes: List<Pair<KAnnotatedElement, ResourceDescriptor>>) {
-    private val httpMethods = Array(HttpMethod.values().size) {
-        ArrayList<ResourceDescriptor>()
-    }
-    private val resources = HashMap<KAnnotatedElement, ResourceDescriptor>()
 
-    init {
-        for ((first, descriptor) in resourceTypes) {
-            resources[first] = descriptor
-            httpMethods[descriptor.httpMethod.ordinal].add(descriptor)
+    private val resources = resourceTypes.toMap()
+
+    private val httpMethods = resources.values.groupBy { it.httpMethod }.mapValues { (method, descriptors) ->
+        descriptors.groupBy { it.route }.mapValues { (route, descList) ->
+            descList.singleOrNull() ?: error("Route [$method - '$route'] has ${descList.size} descriptors, but only 1 is expected!")
         }
     }
 
@@ -29,11 +24,10 @@ class ResourceDispatcher(val context: ApplicationContext, resourceTypes: List<Pa
     /** Matches an http method and url to an ActionInfo object. Returns null if no match is found.
      */
     fun findDescriptor(httpMethod: String, url: String): ResourceDescriptor? {
-        val httpMethodIndex = httpMethod.asHttpMethod().ordinal
-        val matches = httpMethods[httpMethodIndex].filter { it.matches(url) }
+        val matches = httpMethods[httpMethod.asHttpMethod()]?.filterValues { it.matches(url) }?.values.orEmpty()
 
         return when (matches.size) {
-            1 -> matches[0]
+            1 -> matches.single()
             0 -> null
             else -> throw InvalidRouteException("URL '$url' matches more than single route: ${matches.joinToString { it.route }}")
         }
